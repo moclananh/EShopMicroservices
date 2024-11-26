1e624fbfd7ef2378a45d9fa86758883eec18a1f3
Thanh Check Merg <kristino5223@gmail.com>
2024-11-26 15:10:08 +0700

    Update/Delete Product

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -13,9 +13,12 @@ namespace Catalog.API.Products.GetProductById
             {
                 var result = await sender.Send(new GetProductByIdQuery(id));
                 var response = result.Adapt<GetProductByIdResponse>();
+                if (response.Product is null) {
+                    return Results.BadRequest("Product not found!");
+                }
                 return Results.Ok(response);
             }).WithName("GetProductById")
-            .Produces<GetProductByIdResponse>(StatusCodes.Status201Created)
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Get Product By Id")
             .WithDescription("Get Product By Id");
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.API.Products.CreateProduct;
+using Catalog.API.Products.GetAllProducts;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(
+        Guid Id,
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price,
+        List<string> Category
+        ) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+    public class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<GetAllProductQueryHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductCommandHandler.Handler call with {@query}", command);
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product == null)
+            {
+                return new UpdateProductResult(false);
+            }
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Price = command.Price;
+            product.Category = command.Category;
+            product.ImageFile = command.ImageFile;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
+        }
+    }
+}
