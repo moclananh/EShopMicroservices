21f42bc7e06fa82c2818aa59c4ab4ec31a149a49
Thanh Check Merg <kristino5223@gmail.com>
2024-11-26 11:23:24 +0700

    getAllProductCatalog

---
+++ src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetAllProducts
+{
+    public record GetAllProductResponse(IEnumerable<Product> Products);
+    public class GetAllProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetAllProductQuery());
+                var response = result.Adapt<GetAllProductResponse>();
+                if (response.Products.Count() <= 0)
+                {
+                    return Results.NoContent();
+                }
+                return Results.Ok(response);
+            }).WithName("GetAllProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get All Product")
+            .WithDescription("Get all product list from database");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetAllProducts/GetAllProductHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Catalog.API.Products.GetAllProducts
+{
+    public record GetAllProductQuery() : IQuery<GetAllProductResult>;
+    public record GetAllProductResult(IEnumerable<Product> Products);
+    public class GetAllProductQueryHandler(IDocumentSession session, ILogger<GetAllProductQueryHandler> logger) 
+        : IQueryHandler<GetAllProductQuery, GetAllProductResult>
+    {
+        public async Task<GetAllProductResult> Handle(GetAllProductQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetAllProductQueryHandler.Handler call with {@query}", query);
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetAllProductResult(products);
+        }
+    }
+}
