4db32ca33a9380953c9e6c5bc1baf54bd13de89a
Thanh Check Merg <kristino5223@gmail.com>
2024-12-06 15:36:04 +0700

    Implemented CRUD Discount Coupones

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -1,6 +1,19 @@
-﻿namespace Discount.Grpc.Data
+﻿
+namespace Discount.Grpc.Data
 {
-    public class DiscountContext
+    public class DiscountContext : DbContext
     {
+        public DbSet<Coupon> Coupones { get; set; } = default!;
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+                );
+        }
     }
 }
--- src/Services/Discount/Discount.Grpc/GlobalUsing.cs
+++ src/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -1,1 +1,8 @@
-﻿
+﻿global using Discount.Grpc.Services;
+global using Microsoft.EntityFrameworkCore;
+global using Discount.Grpc.Models;
+global using Discount.Grpc.Data;
+global using Grpc.Core;
+global using Mapster;
+
+
