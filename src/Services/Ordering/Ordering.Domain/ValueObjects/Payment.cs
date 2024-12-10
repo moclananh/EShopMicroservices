03eba198c67024b5eb718639e1554cb12656bba6
Thanh Check Merg <kristino5223@gmail.com>
2024-12-10 10:55:02 +0700

    Strongly type IDs

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string? CardNumber { get; } = default!;
+        public string? Expriration { get; } = default!;
+        public string? CVV { get; } = default!;
+        public int PaymentMethod {  get; } = default!;
+    }
+}
