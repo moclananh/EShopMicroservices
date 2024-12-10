4ae74548b7dedd05e78b32549c4ba3b34f6d5e1a
Thanh Check Merg <kristino5223@gmail.com>
2024-12-10 14:27:39 +0700

    Completed config in Ordering.Domain

--- src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -1,4 +1,5 @@
 ﻿namespace Ordering.Domain.Exceptions
+{ 
     public class DomainException : Exception
     {
         public DomainException(string message) : base($"Domain Exception: \"{message}\" throws from Domain layer") 
