3dbe4b44b687eca0865f3897d3a70142686b2205
Thanh Check Merg <kristino5223@gmail.com>
2024-12-17 15:52:29 +0700

    Create WebApp RazorPage

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Catalog
+{
+    public record GetAllProductResponse(IEnumerable<ProductModel> Products);
+    public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+    public record GetProductByIdResponse(ProductModel Product);
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+        public List<string> Category { get; set; } = new();
+    }
+}
