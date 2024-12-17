52791d02af1dcfc774526c86134c0bede7e5f25e
Thanh Check Merg <kristino5223@gmail.com>
2024-12-17 13:56:21 +0700

    Rate Limitting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
@@ -5,7 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 //config yarp api gateway
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+//config limit rate (prevented too many request/per time)
+builder.Services.AddRateLimiter(rateLimitOption =>
+{
+    rateLimitOption.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
+
 //configured HTTP request pipeline
 app.MapReverseProxy();
 app.MapGet("/", () => "Hello World!");
